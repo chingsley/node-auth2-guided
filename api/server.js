5e1f4267e168c278ddfdd6ec7eaf1deaed4a7e4e
chingsley <eneja.kc@gmail.com>
2020-02-26 14:36:56 +0100

    - add session cookies, and use it to protect the route GET /users

--- api/server.js
+++ api/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'monkey',
+  secret: 'keep it secret, keep it safe!',
+  cookie: {
+    maxAge: 1000 * 120, // lasts for 120 seconds = 2 minutes
+    secure: false, // true in production,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: true, // GDPR laws against setting cookies automatically. False for production, and only set it to true if user agrees to that setting
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,6 +25,8 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
+        console.log(req.session);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
